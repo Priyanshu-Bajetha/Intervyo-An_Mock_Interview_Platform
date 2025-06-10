d7b8c57186e25c8611acad892f20368b2d9cc88e
PRIYANSHU BAJETHA <126591587+Priyanshu-Bajetha@users.noreply.github.com>
2025-06-10 16:43:24 +0530

    Develop Auth Pages

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 
-const layout = () => {
+const Authlayout = ({children}: {children: ReactNode}) => {
   return (
-    <div>layout</div>
+    <div className='auth-layout'>{children}</div>
   )
 }
 
-export default layout
+export default Authlayout
